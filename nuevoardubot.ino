345183bc83b5bed7d6931bbadd9232991ea6b8d8
Vanessaxsaez <vanessa.saez@usach.cl>
2018-08-03 04:19:25 -0400

    Final
    Avanza
    detiene cada vez que pasa el limite
    Dobla al detenerse

--- nuevoardubot.ino
+++ nuevoardubot.ino
@@ -2,14 +2,18 @@
 //Pines de ultrasonido
 const int pinecho = 10;
 const int pintrigger = 9;
-//Pines de motorreductor
 
-int PinIN1 = 4;
-int PinIN2 = 5;
+//Pines de motorreductor
+//Motor izquierdo 4,5,2
+//Motor derecho 6,7,3
+int PinIN1 = 4;  
+int PinIN2 = 5;  
 int PinIN3 = 6;
 int PinIN4 = 7;
-int ENB = 3; // ENB conectada al pin 3 de Arduino
-int ENA = 2;
+//Pines controlar la velocidad
+int ENB = 3; 
+int ENA = 2;   
+
 // VARIABLES PARA CALCULOS
 unsigned int tiempo, distancia;
 NewPing sonar(pintrigger, pinecho, 200);
@@ -24,19 +28,17 @@ void setup() {
   pinMode(PinIN4, OUTPUT);
 
 }
-
-
+//Funcion para avanzar
 void avanzar(int vela, int velb)
 {
-  analogWrite(ENB,velb);
   analogWrite(ENA,vela);
+  analogWrite(ENB,velb);
   digitalWrite (PinIN1, LOW);
   digitalWrite (PinIN2, HIGH);
   digitalWrite (PinIN3, HIGH);
   digitalWrite (PinIN4, LOW);
-  
-  
-}
+  }
+
 void detener()
 {
     
@@ -47,8 +49,9 @@ void detener()
 }
 void derecha(int vela, int velb)
 {
+  
+  analogWrite(ENB,velb);
   analogWrite(ENA,vela);
-    analogWrite(ENB,velb);
   digitalWrite (PinIN1, LOW);
   digitalWrite (PinIN2, HIGH);
   digitalWrite (PinIN3, LOW);
@@ -60,8 +63,8 @@ void izquierda(int vela, int velb)
     analogWrite(ENB,velb);
   digitalWrite (PinIN1, LOW);
   digitalWrite (PinIN2, HIGH);
-  digitalWrite (PinIN3, LOW);
-  digitalWrite (PinIN4, HIGH);
+  digitalWrite (PinIN3, HIGH);
+  digitalWrite (PinIN4, LOW);
 }
 
 //calculo de la distancia para sensor
@@ -80,37 +83,31 @@ int ping(int pintrigger, int pinecho) {
   return distanceCm;
 }
 
-//condiciones para que avance o retroceda segun la distancia del sensor
+//condiciones para que avance o se detenga segun la distancia del sensor
 
 void loop() {
 long distanceCm;
   distanceCm = sonar.ping_cm();  
   Serial.print("Ping: ");
-  Serial.print(distanceCm); // Send ping, get distance in cm and print result (0 = outside set distance range)
+  Serial.print(distanceCm); 
   Serial.println("cm");
   delay(25);
   int Limite = 30 ;                  // Medida en vacío del sensor
-//si no cumple la distancia avanza o retrocede
-  /*if (distancia > Limite) {
-    MotorHorario();
-    }
-  else {
-    MotorStop();
-  }  
-  }*/
 
-  if (distanceCm > Limite) {
-     avanzar(80,80);
+//si no cumple la distancia avanza o se detiene y dobla a la derecha
+  
+ if (distanceCm > Limite) {
+     avanzar(130,130);  //minimo 130 al izquierdo, o no funciona el motor
   delay(200);
 }
   
    else {
    detener();
    delay(1000);
-   derecha(170,190);
+   derecha(250,220);
    delay(400);
        if (distancia > Limite) {
-       avanzar(20,20);
+       avanzar(130,130);
        delay(200);
       }
        else {
